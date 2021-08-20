a90fdccabdf680895641b6e238245131c11cdfae
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-08-20 17:24:45 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
+import { ApiService } from "src/app/service/api.service";
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,10 @@ export class AppComponent {
   title = 'wevouch';
   public showHeaderFooterSidebar: boolean = false;
   public loginRegistration: boolean = false;
+  public userDetails: any = {};
+  public notifications: any = '';
 
-  constructor(private _router: Router) {
+  constructor(private _router: Router, private _api:ApiService) {
     _router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
         if (event['url'] == '/login' || event['url'] == '/registration') {
@@ -26,8 +29,17 @@ export class AppComponent {
   }
 
   ngOnInit(){
+    console.log('hello 2');
+    
     if (!localStorage.getItem('we_vouch_user')) {
       this._router.navigate(['/login']);
     }
+    this.userDetails = JSON.parse(localStorage.getItem('we_vouch_user'));
+    this._api.notificationList(this.userDetails._id).subscribe (
+      res => {
+        this.notifications = res;
+        console.log(res);
+      }
+    )
   }
 }
